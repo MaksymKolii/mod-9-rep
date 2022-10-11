5f8c55d09de4c6a73ff991c6a1f9075f782b1734
MaksymKolii <104693134+MaksymKolii@users.noreply.github.com>
2022-10-11 15:26:14 +0000

    Deploying to gh-pages from @ MaksymKolii/mod-9-rep@6d841af4caf52761cb496006191d4d1e66f12c0c 🚀

--- 101.8d9bfa88.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".js-search-form"),document.querySelector(".js-articles-container")][0].addEventListener("submit",(function(e){e.preventDefault(),fetch("https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page1",{headers:{Authorization:"6be54aba098d4ee4bc21992dc9b3c0d5"}}).then((e=>e.json())).then(console.log())}));
-//# sourceMappingURL=101.8d9bfa88.js.map
