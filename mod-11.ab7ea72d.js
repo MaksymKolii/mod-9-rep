ec388fd51ed0a608b578a6bc13774f50b4b7718c
MaksymKolii <104693134+MaksymKolii@users.noreply.github.com>
2022-10-16 20:13:52 +0000

    Deploying to gh-pages from @ MaksymKolii/mod-9-rep@7f8250b1ffd08c56f2e3da1247a2f07e2a759bd5 🚀

--- mod-11.ab7ea72d.js
+++
@@ -1,2 +0,0 @@
-const t={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({title:"Тестовая книга по CSS",author:"Максим",genres:["CSS"],rating:9})};fetch("http://localhost:4040/books",t).then((t=>t.json())).then(console.log);
-//# sourceMappingURL=mod-11.ab7ea72d.js.map
---
+++ mod-11.ce794cc7.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=mod-11.ce794cc7.js.map
