67be41cf666a6e1b5989bb79a7806004965f1f00
Maksym Kolii <maksymkolii@gmail.com>
2022-10-16 13:16:37 +0200

    commit

---
+++ src/js/mod-11.js
@@ -0,0 +1,9 @@
+
+
+// Т.к. мы установили json-server  в документации написано запустить json-server --watch db.json
+// мы в package.jsone в поле "scripts"  создадим элемент например с именем "server" и пропишем то что хотят json-server --watch db.json
+// Итого будет в package.jsone ---   "server":"json-server --watch db.json"
+
+// Далее вызываем npm run server
+
+
